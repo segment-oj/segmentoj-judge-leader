72552c28e5acaaab23d70d8d450ff9aaa02d9ff9
zhangtianli2006 <zhangtianli2006@163.com>
2021-02-10 12:42:29 +0800

    [feature] task queue
    Signed-off-by: zhangtianli2006 <zhangtianli2006@163.com>

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { login } from './login';
 import { get_status } from './server';
 import { Task } from './task';
 
+let task_queue = Array<Task>();
+
 const app = express();
 const config: Config = get_config();
 const io = require('socket.io-client');
@@ -18,15 +20,15 @@ login(config)
     socket.emit('set-priority', 0);
 
     // Get task
-    socket.on('assign-task', (task_id) => {
+    socket.on('assign-task', task_id => {
         get_status(config, task_id)
         .then(res => {
-            console.log(res);
+            task_queue.push(res);
         })
         .catch(err => {
             console.log(`[Task] get task from server ERR ${err.request.status}`);
         });
-    });;
+    });
 
     app.listen(config.port, () => {
         console.log(`Serving on port ${config.port}`);
